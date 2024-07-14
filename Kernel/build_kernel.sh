b1f5ac8bacd75f3403af21d30895b1e43fe8f722
mylove90 <engm.saad1990@gmail.com>
2024-07-14 22:21:17 +0000

    Build_kernel.sh: Use python2

--- Kernel/build_kernel.sh
+++ Kernel/build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd kernel-5.10
-python scripts/gen_build_config.py --kernel-defconfig a15_kor_00_defconfig --kernel-defconfig-overlays entry_level.config -m user -o ../out/target/product/a15_kor/obj/KERNEL_OBJ/build.config
+python2 scripts/gen_build_config.py --kernel-defconfig a15_kor_00_defconfig --kernel-defconfig-overlays entry_level.config -m user -o ../out/target/product/a15_kor/obj/KERNEL_OBJ/build.config
 
 export ARCH=arm64
 export CROSS_COMPILE="aarch64-linux-gnu-"
